fbc6ee9314fa89eff6d0ffec01c211fdee3b3513
qibang108 <53903949+qibang108@users.noreply.github.com>
2020-02-28 22:53:05 +0800

    Create helloworld.asm

---
+++ helloworld.asm
@@ -0,0 +1,20 @@
+.386
+
+data segment use16
+buf DB 'hello, world! $'
+data ends
+
+code segment use16
+assume cs:code, ds:data
+
+start:
+	mov ax,data
+	mov ds,ax
+	LEA dx,buf
+	mov ah,9
+	int 21H
+	mov ah,4ch
+	int 21H
+
+code ends
+end start
